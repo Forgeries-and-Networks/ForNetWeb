d7d7b7e14b6ddea1edd4e4358d80993eab5841e3
Demi Vasques <dvasques83@users.noreply.github.com>
2025-02-28 09:29:32 +0100

    trying new approache to footer

---
+++ docs/.vitepress/theme/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer class="custom-footer">
+    <div class="logos">
+      <a href="https://path-to-logo1-link" target="_blank">
+        <img src="/public/logo1.png" alt="Logo 1" />
+      </a>
+      <a href="https://path-to-logo2-link" target="_blank">
+        <img src="/public/logo2.png" alt="Logo 2" />
+      </a>
+    </div>
+    <p>© 2023 Forgeries and Networks. All rights reserved.</p>
+  </footer>
+</template>
+
+<style scoped>
+.custom-footer {
+  text-align: center;
+  padding: 20px;
+  border-top: 1px solid #eaecef;
+  margin-top: 40px;
+}
+.logos img {
+  height: 40px;
+  margin: 0 10px;
+  vertical-align: middle;
+}
+.logos a {
+  display: inline-block;
+}
+</style>
