0592abb144b1ffe669c9e8ffe5ec646782965e3e
Demi Vasques <dvasques83@users.noreply.github.com>
2025-02-28 11:01:59 +0100

    Update Footer.vue trying minimal example

--- docs/.vitepress/theme/components/Footer.vue
+++ docs/.vitepress/theme/components/Footer.vue
@@ -1,30 +1,14 @@
 <template>
-  <footer class="custom-footer">
-    <div class="logos">
-      <a href="https://www.ieg-mainz.de/likecms.php?function=set_lang&lang=en" target="_blank">
-        <img src="/logo_IEG/IEG_Logo_Vektorgrafik_mit-Schriftzug_Leibniz.jpg" alt="Logo 1" />
-      </a>
-      <a href="https://www.ieg-mainz.de/likecms.php?function=set_lang&lang=en" target="_blank">
-        <img src="/logo_IEG/IEG_Logo_Vektorgrafik_mit-Schriftzug_Leibniz.jpg" alt="Logo 2" />
-      </a>
-    </div>
-    <p>© 2023 Forgeries and Networks. All rights reserved.</p>
+  <footer>
+    <p>This is a minimal footer.</p>
   </footer>
 </template>
 
 <style scoped>
-.custom-footer {
+footer {
   text-align: center;
   padding: 20px;
   border-top: 1px solid #eaecef;
   margin-top: 40px;
-}
-.logos img {
-  height: 40px;
-  margin: 0 10px;
-  vertical-align: middle;
-}
-.logos a {
-  display: inline-block;
 }
 </style>
