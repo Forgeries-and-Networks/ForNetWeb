4e43ca9dac2bbb805e4c7ce17917859ceab0dddc
Demi Vasques <dvasques83@users.noreply.github.com>
2025-02-28 09:48:52 +0100

    Update Layout.vue

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,12 +1,13 @@
 <script setup>
 import DefaultTheme from 'vitepress/theme';
+import Footer from './components/Footer.vue'; // Import the Footer component
 const { Layout: DefaultLayout } = DefaultTheme;
 </script>
 
 <template>
   <DefaultLayout>
     <template #content-bottom>
-      <Footer />
+      <Footer /> <!-- Use the Footer component -->
     </template>
   </DefaultLayout>
 </template>
